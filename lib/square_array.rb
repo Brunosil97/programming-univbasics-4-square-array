9e49165ad75dd3fc6184c6c859328e6f304a6261
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-07 15:00:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -12,4 +12,14 @@ end
 # ** 2 means square rooting to the power two. The second power 
 # line 7 means number on the left being multiplied by the right
 
+#def square_array(array)
+# results = []
+# counter = 0 
+# while array[counter] do 
+# array[counter] * array[counter] << results
+ #  counter +=1 
+ # end
+#return results 
+#end
 
+#this is wrong 
