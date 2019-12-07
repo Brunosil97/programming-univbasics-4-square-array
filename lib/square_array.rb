8d7b19905d07009a556f687882bbb7430a87d048
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-07 15:01:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
  results = []
  counter = 0 
  while array[counter] do 
- results << array[counter] * array[counter] 
+ results << array[counter] * array[counter] #fixed it by changing which way 
    counter +=1 
   end
 return results 
