d498556500958af3bfcc7a92e37017ab8c34d7df
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-07 14:48:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -12,4 +12,4 @@ end
 # ** 2 means square rooting to the power two. The second power 
 # line 7 means number on the left being multiplied by the right
 
-square_array << results 
+square_array(numbers) << results 
