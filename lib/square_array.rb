8029edea99388baadd45064a24e1bfb4f2c708f8
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-07 14:53:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,12 @@
-results = []
-
 
 def square_array(array)
+ results = []
  counter = 0 
  while array[counter] do 
- array[counter] * array[counter]
+ array[counter] * array[counter] << results 
    counter +=1 
   end
+  return results 
 end
 
 # ** 2 means square rooting to the power two. The second power 
